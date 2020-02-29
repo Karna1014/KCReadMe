c8e25988231d95fadbf06b1c70f57e944f3898be
Java K <karna.cutolo@gmail.com>
2020-02-29 09:12:33 -0500

    Working ReadMe with pic

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# KCReadMe
-Node Generated ReadMe
+ ** Github Username: **
+ Karna1014
+
+ #ReadMe!
+
+ ## Description: 
+A command-line application that dynamically generates a ReadMe.md utilizing user input.
+
+ ## Table of Contents: 
+ * Installation 
+ * Usage 
+ * License 
+ * Contributing 
+ * Tests 
+ * Questions 
+ * Images 
+
+ #### Installation: 
+npm i
+
+ ### Usage: 
+As a developer, this project provides a template for readily generating a ReadMe document for a new project.
+
+ ####License: 
+ISC
+
+ ###Contributions: Karna Cutolo
+
+ ** Tests: **
+npm test
+
+### Questions: 
+<img src="https://avatars3.githubusercontent.com/u/57597042?v=4" alt="avatar" style="border-radius: 16px" width="20px" />
+ If you have any questions about this repo, please reach out: 
+ ** Email: **karna.cutolo@gmail.com
+
+ ####Images:
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inquirer = require ("inquirer");
 const axios = require ("axios");
 const promise = require ("promise");
 
+
+
 async function fetchUserInfo () {
 
 const answer = await inquirer
@@ -15,28 +17,95 @@ const answer = await inquirer
     {
     message: "Enter your GitHub username",
     name: "username"
-  }
+    },
+
+    {
+    message: "What is the Title of your project",
+    name: "title"
+    },
+
+    {
+    message: "Give a short description of your project",
+    name: "description"
+    },
+
+    {
+    message: "Installation Information?",
+    name: "installation"
+    },
+
+    {
+    message: "Project Usage?",
+    name: "usage"
+    },
+    {
+      type: "list",
+      message: "License?",
+      name: "license",
+      choices: [
+        "ISC",
+        "MIT",
+        "IBM",
+        "  "
+      ]
+    },
+
+    {
+      message: "Who worked on the project?",
+      name: "contributing"
+    },
+
+    {
+     message:  "Tests required?",
+     name: "tests"
+    }
+
+
   ]);
-    const url = `https://api.github.com/users/${answer.username}/repos?per_page=100`;
+    const url = `https://api.github.com/users/${answer.username}`;
     
     const response = await axios.get(url)
-        .then(function(data) {
-          console.log(data);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
+       
 
-    
-
-
-
-  
+    writeFile(answer, response.data);
     console.log(response.data);
   
 }
 
-fetchUserInfo();
+   fetchUserInfo();
+
+function writeFile(answer, github) {
+const writeStream = fs.createWriteStream('README.md');
+
+const pathName = writeStream.path;
+
+
+
+// write each value of the array on the file breaking line
+writeStream.write(` ** Github Username: **\n ${github.login}\n\n #${answer.title}\n\n ## Description: \n${answer.description}\n\n ## Table of Contents: \n * Installation \n * Usage \n * License \n * Contributing \n * Tests \n * Questions \n * Images \n\n #### Installation: \n${answer.installation}\n\n ### Usage: \n${answer.usage}\n\n #### License: \n${answer.license}: ${licChoice}\n\n ### Contributions: ${answer.contributing}\n\n ** Tests: **\n${answer.tests}\n\n### Questions: \n<img src="${github.avatar_url}" alt="avatar" style="border-radius: 16px" width="20px" />\n If you have any questions about this repo, please reach out: \n ** Email: ** \n${answer.email}\n\n #### Images:`)
+
+// the finish event is emitted when all data has been flushed from the stream
+writeStream.on('finish', () => {
+   console.log(`wrote all the array data to file ${pathName}`);
+});
+
+// handle the errors on the write process
+writeStream.on('error', (err) => {
+    console.error(`There is an error writing the file ${pathName} => ${err}`)
+});
+
+// close the stream
+writeStream.end();
+};
+
+const licChoice = function() {
+  if(answer.license[0] => {`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`);
+  } else (answer.license[1] => {`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`);
+  } else {
+    `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
+  }; 
+};
+
 
 
 
